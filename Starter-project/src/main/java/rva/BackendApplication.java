20cb8df85da463d50eaa89ad5fc74b65224fc28c
Lenka Subotin <subotinlenka@gmail.com>
2021-03-22 01:24:45 +0100

    backend RVA nakon vezbi 2

--- Starter-project/src/main/java/rva/BackendApplication.java
+++ Starter-project/src/main/java/rva/BackendApplication.java
@@ -3,13 +3,12 @@ package rva;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// GitHub Classroom Invite Link --> https://classroom.github.com/a/PgzNRc2E
 
 @SpringBootApplication
-public class StarterProjectApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StarterProjectApplication.class, args); 
+		SpringApplication.run(BackendApplication.class, args); 
 	}
 
 }
