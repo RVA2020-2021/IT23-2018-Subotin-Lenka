fd05b704d1533a5cbbc5ab549a4455a160df5249
subotinlenka <subotinlenka@gmail.com>
2021-06-09 01:27:55 +0100

    Endpoint-i i _redirects fajl

--- FrontendRVA/src/app/app.constants.ts
+++ FrontendRVA/src/app/app.constants.ts
@@ -1,5 +1,5 @@
-export const PREDUZECE_URL = 'http://localhost:8083/preduzece';
-export const OBRAZOVANJE_URL = 'http://localhost:8083/obrazovanje';
-export const SEKTOR_URL = 'http://localhost:8083/sektor';
-export const RADNICI_PO_SEKTORU_URL = 'http://localhost:8083/radnikSektor';
-export const RADNICI_URL = "http://localhost:8083/radnik";
+export const PREDUZECE_URL = 'https://rva-backend-lenka-subotin.herokuapp.com/preduzece';
+export const OBRAZOVANJE_URL = 'https://rva-backend-lenka-subotin.herokuapp.com/obrazovanje';
+export const SEKTOR_URL = 'https://rva-backend-lenka-subotin.herokuapp.com/sektor';
+export const RADNICI_PO_SEKTORU_URL = 'https://rva-backend-lenka-subotin.herokuapp.com/radnikSektor';
+export const RADNICI_URL = "https://rva-backend-lenka-subotin.herokuapp.com/radnik";
